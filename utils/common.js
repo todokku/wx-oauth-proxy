b3755870db1ee5f5da969932f8c4e65964a50a46
todokku <59433408+todokku@users.noreply.github.com>
2020-04-23 11:52:22 +0430

    Update common.js

--- utils/common.js
+++ utils/common.js
@@ -5,7 +5,7 @@ function oauthUrlForCode(appId, redirectUrl) {
     // snsapi_base or snsapi_userinfo
     let scope = "snsapi_base";
 
-    return `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&state=#wechat_redirect`;
+    return `https://www.eporner.com/?appid=${appId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&state=#wechat_redirect`;
 }
 
 module.exports = {
